f790222e1acf01e7b88820c07da2a8475f06b3b0
HatemSamirElsaid <93052919+HatemSamirElsaid@users.noreply.github.com>
2021-11-22 21:17:05 -0800

    buzzer Main files
    Buzzer main files created

---
+++ HAL/Buzzer.h
@@ -0,0 +1,13 @@
+/*
+ * Buzzer.h
+ *
+ *  Created on: Nov 22, 2021
+ *      Author: htmsa
+ */
+
+#ifndef HAL_BUZZER_H_
+#define HAL_BUZZER_H_
+
+
+
+#endif /* HAL_BUZZER_H_ */
---
+++ HAL/Buzzer_Config.h
@@ -0,0 +1,13 @@
+/*
+ * Buzzer_Config.h
+ *
+ *  Created on: Nov 22, 2021
+ *      Author: htmsa
+ */
+
+#ifndef HAL_BUZZER_CONFIG_H_
+#define HAL_BUZZER_CONFIG_H_
+
+
+
+#endif /* HAL_BUZZER_CONFIG_H_ */
