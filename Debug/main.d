54b37c95a12435fc4dd301e2cd1dc03da4975eb3
HatemSamirElsaid <93052919+HatemSamirElsaid@users.noreply.github.com>
2021-11-23 10:54:43 -0800

    Button Module
    Button Module is finished but not tested yet.

---
+++ Debug/main.d
@@ -0,0 +1,32 @@
+main.o main.o: ../main.c ../MCAL.h ../MCAL/Dio.h ../MCAL/Dio_Types.h \
+  ../MCAL/Dio_Config.h ../HAL.h ../HAL/LED.h ../HAL/LED_Types.h \
+  ../HAL/LED_Config.h ../HAL/Button.h ../HAL/Button_Config.h \
+  ../HAL/Button_Types.h ../Lib.h ../Lib/Bit_Math.h ../Lib/Std_Types.h
+
+../MCAL.h:
+
+../MCAL/Dio.h:
+
+../MCAL/Dio_Types.h:
+
+../MCAL/Dio_Config.h:
+
+../HAL.h:
+
+../HAL/LED.h:
+
+../HAL/LED_Types.h:
+
+../HAL/LED_Config.h:
+
+../HAL/Button.h:
+
+../HAL/Button_Config.h:
+
+../HAL/Button_Types.h:
+
+../Lib.h:
+
+../Lib/Bit_Math.h:
+
+../Lib/Std_Types.h:
