37bbe3b92ecd5b61d3ce663ee7f16a64f752a420
HatemSamirElsaid <93052919+HatemSamirElsaid@users.noreply.github.com>
2021-11-23 10:11:28 -0800

    Buzzer Module Tested
    Buzzer Module is tested

--- main.c
+++ main.c
@@ -28,6 +28,22 @@ int main ()
 	LED_Init(LED1);
 	LED_Init(LED2);*/
 	//Block2End
+
+
+	//Block3Start	HAL LED Buzzer
+		DIO_WRITE_STATUS(DIO_PORTC, DIO_PIN2, OTUPUT);//Button1
+		DIO_WRITE_STATUS(DIO_PORTC, DIO_PIN7, OTUPUT);//Button2
+		DIO_WRITE_STATUS(DIO_PORTD, DIO_PIN3, OTUPUT);//Button3
+		Buzzer_Init();
+
+
+		LED_Init(LED0);
+		LED_Init(LED1);
+		LED_Init(LED2);
+		Boolean Flag1 =FALSE;
+		Boolean Flag2 =FALSE;
+		Boolean Flag3 =FALSE;
+		//Block3End
 	while (1)
 	{
 		//Block1Start	MCAL
@@ -73,6 +89,35 @@ int main ()
 		LED_OFF(LED1);
 		_delay_ms(75);*/
 		//Block2End
+
+		//Block3Start	HAL LED Buzzer
+		if (DIO_READ_LEVEL(DIO_PORTB, DIO_PIN0)==HIGH && Flag1==FALSE){
+			LED_TOGGLE(LED0);
+			Buzzer_ON();
+			Flag1 = TRUE;
+		}else if(DIO_READ_LEVEL(DIO_PORTB, DIO_PIN0)==LOW && Flag1==TRUE){
+			Flag1=FALSE;
+			_delay_ms(10);
+		}
+
+		if (DIO_READ_LEVEL(DIO_PORTB, DIO_PIN4)==HIGH && Flag2==FALSE){
+			LED_TOGGLE(LED1);
+			Buzzer_OFF();
+			Flag2 = TRUE;
+		}else if(DIO_READ_LEVEL(DIO_PORTB, DIO_PIN4)==LOW && Flag2==TRUE){
+			Flag2=FALSE;
+			_delay_ms(10);
+		}
+
+		if (DIO_READ_LEVEL(DIO_PORTD, DIO_PIN2)==HIGH && Flag3==FALSE){
+			LED_TOGGLE(LED2);
+			Buzzer_TOGGLE();
+			Flag3 = TRUE;
+		}else if(DIO_READ_LEVEL(DIO_PORTD, DIO_PIN2)==LOW && Flag3==TRUE){
+			Flag3=FALSE;
+			_delay_ms(10);
+		}
+		//Block3End
 	}
 
 	return 0;
